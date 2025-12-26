bb2a8af5aa2fb665abdd801e5954d83edb01ab6b
yaghesh369 <vyasyaghesh001@gmail.com>
2025-12-26 22:05:15 +0530

    hhsjs

--- studybuddy/wsgi.py
+++ studybuddy/wsgi.py
@@ -1,6 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybuddy.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "studybuddy.settings")
 
 application = get_wsgi_application()
+
+# 🔥 REQUIRED FOR VERCEL
+app = application
+handler = application
